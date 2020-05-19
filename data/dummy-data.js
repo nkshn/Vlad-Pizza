0f7f17d9646f53b24d6f2efe4ee95270ff554564
nkshn <vovanika23413@gmail.com>
2020-05-19 23:38:47 +0300

    Changed data and model of product

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -33,77 +33,80 @@ export const SUB_CATEGORIES = [
 
 export const PRODUCTS = [
   // Основна кат.: [Піцца], Другорядна кат.: [Краща ціна]
-  new Product('p1', 'sc1', 'Маргарита', 85),
-  new Product('p2', 'sc1', 'Грибна', 85),
-  new Product('p3', 'sc1', 'Два сири', 85),
-  new Product('p4', 'sc1', 'Вегетаріанська', 85),
+  new Product('p1', 'sc1', 'Маргарита', true, true, true, '25cм', '32cм', '40cм', 85, 165, 330),
+  new Product('p2', 'sc1', 'Грибна', true, true, true, '25cм', '32cм', '40cм', 85, 165, 330),
+  new Product('p3', 'sc1', 'Два сири', true, true, true, '25cм', '32cм', '40cм', 85, 165, 330),
+  new Product('p4', 'sc1', 'Вегетаріанська', true, true, true, '25cм', '32cм', '40cм', 85, 165, 330),
 
   // Основна кат.: [Піцца], Другорядна кат.: [Класичні]
-  new Product('p5', 'sc2', 'Мисливська', 75),
-  new Product('p6', 'sc2', 'Куряча', 75),
-  new Product('p7', 'sc2', 'Салямі', 75),
-  new Product('p8', 'sc2', 'Гавайська', 75),
-  new Product('p9', 'sc2', 'Техас', 75),
-  new Product('p10', 'sc2', 'Шинка-Ананас', 75),
+  new Product('p5', 'sc2', 'Мисливська', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
+  new Product('p6', 'sc2', 'Куряча', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
+  new Product('p7', 'sc2', 'Салямі', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
+  new Product('p8', 'sc2', 'Гавайська', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
+  new Product('p9', 'sc2', 'Техас', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
+  new Product('p10', 'sc2', 'Шинка-Ананас', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
 
   // Основна кат.: [Піцца], Другорядна кат.: [Фірмові]
-  new Product('p11', 'sc3', 'Шинка-Гриби', 75),
-  new Product('p12', 'sc3', 'З Беконом', 75),
+  new Product('p11', 'sc3', 'Шинка-Гриби', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
+  new Product('p12', 'sc3', 'З Беконом', true, true, true, '25cм', '32cм', '40cм', 75, 150, 300),
 
   // Основна кат.: [Піцца], Другорядна кат.: [Легенди]
-  new Product('p13', 'sc4', 'Курка з грибами', 85),
-  new Product('p14', 'sc4', 'Домашня', 85),
+  new Product('p13', 'sc4', 'Курка з грибами', true, true, true, '25cм', '32cм', '40cм', 85, 165, 330),
+  new Product('p14', 'sc4', 'Домашня', true, true, true, '25cм', '32cм', '40cм', 85, 165, 330),
 
   // Основна кат.: [Піцца], Другорядна кат.: [Преміум]
-  new Product('p15', 'sc5', 'Неаполітано', 90),
-  new Product('p16', 'sc5', 'Американо', 90),
+  new Product('p15', 'sc5', 'Неаполітано', true, true, true, '25cм', '32cм', '40cм', 90, 180, 360),
+  new Product('p16', 'sc5', 'Американо', true, true, true, '25cм', '32cм', '40cм', 90, 180, 360),
 
   // Основна кат.: [Піцца], Другорядна кат.: [Chef`s Best]
-  new Product('p17', 'sc6', 'Морська', 200),
-  new Product('p18', 'sc6', 'Мюнхенська', 200),
-  new Product('p19', 'sc6', 'Чотири Сири', 200),
+  new Product('p17', 'sc6', 'Морська', false, true, true, null, '32cм', '40cм', null, 200, 400),
+  new Product('p18', 'sc6', 'Мюнхенська', false, true, true, null, '32cм', '40cм', null, 200, 400),
+  new Product('p19', 'sc6', 'Чотири Сири', false, true, true, null, '32cм', '40cм', null, 200, 400),
+
 
   // Основна кат.: [Страви], Другорядна кат.: [Картопля]
-  new Product('p20', 'sc7', 'Картопля по-селянськи', 35),
-  new Product('p21', 'sc7', 'Картопля Фрі', 35),
+  new Product('p20', 'sc7', 'Картопля по-селянськи', true, false, false, '150г', null, null, 35, null, null),
+  new Product('p21', 'sc7', 'Картопля Фрі', true, false, false, '150г', null, null, 35, null, null),
 
   // Основна кат.: [Страви], Другорядна кат.: [Салати]
-  new Product('p22', 'sc8', 'Грецький', 65),
-  new Product('p23', 'sc8', 'Цезар', 85),
-  new Product('p24', 'sc8', '“Салат з мідіями', 95),
+  new Product('p22', 'sc8', 'Грецький', true, false, false, '250г', null, null, 65, null, null),
+  new Product('p23', 'sc8', 'Цезар', true, false, false, '250г', null, null, 85, null, null),
+  new Product('p24', 'sc8', 'Салат з мідіями', true, false, false, '250г', null, null, 95, null, null),
+
 
   // Основна кат.: [Напої], Другорядна кат.: [Напої]
-  new Product('p25', 'sc9', 'Pepsi', 10),
-  new Product('p26', 'sc9', 'Sandora', 25),
-  new Product('p27', 'sc9', 'Jaffa', 10),
-  new Product('p28', 'sc9', 'Pine Water', 15),
-  new Product('p29', 'sc9', 'Lipton Ice Tea', 15),
-  new Product('p30', 'sc9', 'Ice coffe', 35),
+  new Product('p25', 'sc9', 'Pepsi', true, true, true, '0,33л', '0,5л', '1л', 10, 15, 25),
+  new Product('p26', 'sc9', 'Sandora', false, true, true, null, '0,5л', '1л', null, 25, 35),
+  new Product('p27', 'sc9', 'Jaffa', true, false, false, '125мл', null, null, 10, null, null),
+  new Product('p28', 'sc9', 'Pine Water', true, false, false, '0,33л', null, null, 15, null, null),
+  new Product('p29', 'sc9', 'Lipton Ice Tea', false, true, true, null, '0,5л', '1л', false, 15, 20),
+  new Product('p30', 'sc9', 'Ice coffe', true, false, false, '125мл', null, null, 35, null, null),
 
   // Основна кат.: [Напої], Другорядна кат.: [Кава]
-  new Product('p31', 'sc10', 'Еспресо', 10),
-  new Product('p32', 'sc10', 'Американо', 10),
-  new Product('p33', 'sc10', 'Лате', 20),
-  new Product('p34', 'sc10', 'Капучино', 20),
-  new Product('p35', 'sc10', 'Флет Вайт', 25),
-  new Product('p36', 'sc10', 'РАФ', 25),
-  new Product('p37', 'sc10', 'Какао', 20),
-  new Product('p38', 'sc10', 'M&M`s', 40),
-  new Product('p39', 'sc10', 'Snickers', 40),
-  new Product('p40', 'sc10', 'Oreo', 40),
-  new Product('p41', 'sc10', 'Айс Лате', 35),
+  new Product('p31', 'sc10', 'Еспресо', false, false, false, null, null, null, 10, null, null),
+  new Product('p32', 'sc10', 'Американо', false, false, false, null, null, null, 10, null, null),
+  new Product('p33', 'sc10', 'Лате', false, false, false, null, null, null, 20, null, null),
+  new Product('p34', 'sc10', 'Капучино', false, false, false, null, null, null, 20, null, null),
+  new Product('p35', 'sc10', 'Флет Вайт', false, false, false, null, null, null, 25, null, null),
+  new Product('p36', 'sc10', 'РАФ', false, false, false, null, null, null, 25, null, null),
+  new Product('p37', 'sc10', 'Какао', false, false, false, null, null, null, 20, null, null),
+  new Product('p38', 'sc10', 'M&M`s', false, false, false, null, null, null, 40, null, null),
+  new Product('p39', 'sc10', 'Snickers', false, false, false, null, null, null, 40, null, null),
+  new Product('p40', 'sc10', 'Oreo', false, false, false, null, null, null, 40, null, null),
+  new Product('p41', 'sc10', 'Айс Лате', false, false, false, null, null, null, 30, null, null),
 
   // Основна кат.: [Напої], Другорядна кат.: [Чай]
-  new Product('p42', 'sc11', 'Чай', 10),
+  new Product('p42', 'sc11', 'Чай', false, false, false, null, null, null, 10, null, null),
+
 
   // Основна кат.: [Десерти], Другорядна кат.: [Морозиво]
-  new Product('p43', 'sc12', 'Лайм', 12),
-  new Product('p44', 'sc12', 'Гранат', 12),
-  new Product('p45', 'sc12', 'Бренді Кава', 12),
-  new Product('p46', 'sc12', 'Black Ice', 12),
-  new Product('p47', 'sc12', 'Фісташкове', 12),
-  new Product('p48', 'sc12', 'Лісові Ягоди', 12),
-  new Product('p49', 'sc12', 'Пломбір', 12),
-  new Product('p50', 'sc12', 'Бабл-Гам', 12),
-  new Product('p51', 'sc12', 'Ананасове', 12),
+  new Product('p43', 'sc12', 'Лайм', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p44', 'sc12', 'Гранат', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p45', 'sc12', 'Бренді Кава', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p46', 'sc12', 'Black Ice', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p47', 'sc12', 'Фісташкове', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p48', 'sc12', 'Лісові Ягоди', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p49', 'sc12', 'Пломбір', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p50', 'sc12', 'Бабл-Гам', true, false, false, '1 кулька', null, null, 12, null, null),
+  new Product('p51', 'sc12', 'Ананасове', true, false, false, '1 кулька', null, null, 12, null, null),
 ];
--- models/product.js
+++ models/product.js
@@ -1,10 +1,41 @@
 class Product {
-  constructor(ID, ID_subCategory, name, price) {
+  constructor(
+    // Basic info
+    ID,
+    ID_subCategory,
+    name,
+    // price,
+    // For correct layout
+    isSmallSize,
+    isMediumSize,
+    isLargeSize,
+    // Names
+    smallSizeName,
+    mediumSizeName,
+    largeSizeName,
+    // Prices
+    smallSizePrice,
+    mediumSizePrice,
+    largeSizePrice
+  ) {
+    // Basic info
     this.ID = ID;
     this.ID_subCategory = ID_subCategory;
     this.name = name;
     this.price = price;
+    // For correct layout
+    this.isSmallSize = isSmallSize;
+    this.isMediumSize = isMediumSize;
+    this.isLargeSize = isLargeSize;
+    // Names
+    this.smallSizeName = smallSizeName;
+    this.mediumSizeName = mediumSizeName;
+    this.largeSizeName = largeSizeName;
+    // Prices
+    this.smallSizePrice = smallSizePrice;
+    this.mediumSizePrice = mediumSizePrice;
+    this.largeSizePrice = largeSizePrice;
   }
-};
+}
 
 export default Product;
